ce5b4aa6d0756b6c6b43d0ac7f2f6ab7cf4cfa7f
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-11 21:16:20 +0700

    Update Post.java

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -6,6 +6,7 @@ public class Post {
     private int date;  //дата публикации
     private int friends;  //для друзей
     private Comments comements;  //комментарии
+    private Likes likesl; // отметка нравится
     private int anchor;  //закрепить запись
     private int del;  //удаление
     private int edit; //редактирование
