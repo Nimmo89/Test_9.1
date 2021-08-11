ee64533bb58c19a58885a21f5b87b5c7559d0d55
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-11 21:08:55 +0700

    add post.java v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;  //индетификатор поста
+    private int ownerId;  //индетификатор создателя поста
+    private int date;  //дата публикации
+    private int friends;  //для друзей
+    private Comments comements;  //комментарии
+    private int anchor;  //закрепить запись
+    private int del;  //удаление
+    private int edit; //редактирование
+    private boolean favorite;  //закладка, избранное
+    
+}
