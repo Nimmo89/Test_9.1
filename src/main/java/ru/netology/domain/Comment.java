a6473787039dd0e2f26e7b2ecb5f68078618d2ee
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-13 22:41:50 +0700

    Update Comment.java

--- src/main/java/ru/netology/domain/Comment.java
+++ src/main/java/ru/netology/domain/Comment.java
@@ -4,4 +4,19 @@ public class Comment {
     private int count;  //счетчик комментариев
     private int canPost;  //доступ к комментарию
 
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getCanPost() {
+        return canPost;
+    }
+
+    public void setCanPost(int canPost) {
+        this.canPost = canPost;
+    }
 }
