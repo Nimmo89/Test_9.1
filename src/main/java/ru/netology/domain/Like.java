ce2aeb2582aa1f720f4f674addc1e9cd6f503c1b
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-13 22:40:55 +0700

    Update Like.java

--- src/main/java/ru/netology/domain/Like.java
+++ src/main/java/ru/netology/domain/Like.java
@@ -4,4 +4,19 @@ public class Like {
     private int count;
     private int userLike;
 
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getUserLike() {
+        return userLike;
+    }
+
+    public void setUserLike(int userLike) {
+        this.userLike = userLike;
+    }
 }
