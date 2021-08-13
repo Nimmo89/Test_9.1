83981dda16689c3291642c9d820c9970cdd1c879
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-13 22:41:26 +0700

    Update Donut.java

--- src/main/java/ru/netology/domain/Donut.java
+++ src/main/java/ru/netology/domain/Donut.java
@@ -6,4 +6,44 @@ public class Donut {
     private Placeholder placeholder; //заглушка для пользователей, которые не оформили подписку VK Donut
     private boolean canPublishFreeCopy; //можно ли открыть запись для всех пользователей, а не только подписчиков VK Donut;
     private String editMode; //информация о том, какие значения VK Donut можно изменить в записи
+
+    public boolean isDonut() {
+        return isDonut;
+    }
+
+    public void setDonut(boolean donut) {
+        isDonut = donut;
+    }
+
+    public int getPaidDuration() {
+        return paidDuration;
+    }
+
+    public void setPaidDuration(int paidDuration) {
+        this.paidDuration = paidDuration;
+    }
+
+    public Placeholder getPlaceholder() {
+        return placeholder;
+    }
+
+    public void setPlaceholder(Placeholder placeholder) {
+        this.placeholder = placeholder;
+    }
+
+    public boolean isCanPublishFreeCopy() {
+        return canPublishFreeCopy;
+    }
+
+    public void setCanPublishFreeCopy(boolean canPublishFreeCopy) {
+        this.canPublishFreeCopy = canPublishFreeCopy;
+    }
+
+    public String getEditMode() {
+        return editMode;
+    }
+
+    public void setEditMode(String editMode) {
+        this.editMode = editMode;
+    }
 }
