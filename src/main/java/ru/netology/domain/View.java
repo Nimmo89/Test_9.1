b196e7b05c4c8efc1cae3f46d6e14450c1277962
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-13 22:37:40 +0700

    Update View.java

--- src/main/java/ru/netology/domain/View.java
+++ src/main/java/ru/netology/domain/View.java
@@ -2,4 +2,12 @@ package ru.netology.domain;
 
 public class View {
     private int count;
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
