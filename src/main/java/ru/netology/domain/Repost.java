a9e935214c50a92ea938d6001c79128330cd4194
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-13 22:38:59 +0700

    Update Repost.java

--- src/main/java/ru/netology/domain/Repost.java
+++ src/main/java/ru/netology/domain/Repost.java
@@ -3,4 +3,20 @@ package ru.netology.domain;
 public class Repost {
     private int count;
     private int userRepost;
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getUserRepost() {
+        return userRepost;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setUserRepost(int userRepost) {
+        this.userRepost = userRepost;
+    }
 }
