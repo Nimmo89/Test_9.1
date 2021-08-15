2bdff3853b76ee857f48466c1caad44314de56d0
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-15 12:34:00 +0700

    Update Like.java

--- src/main/java/ru/netology/domain/Like.java
+++ src/main/java/ru/netology/domain/Like.java
@@ -1,6 +1,7 @@
 package ru.netology.domain;
 
 public class Like {
+    //v1
     private int count;
     private int userLike;
 
@@ -18,5 +19,26 @@ public class Like {
 
     public void setUserLike(int userLike) {
         this.userLike = userLike;
+    }
+
+
+    //v2
+    private int canLike; //информация о том, может ли текущий пользователь поставить отметку «Мне нравится»
+    private int canPublish; //информация о том, может ли текущий пользователь сделать репост записи
+
+    public int getCanLike() {
+        return canLike;
+    }
+
+    public void setCanLike(int canLike) {
+        this.canLike = canLike;
+    }
+
+    public int getCanPublish() {
+        return canPublish;
+    }
+
+    public void setCanPublish(int canPublish) {
+        this.canPublish = canPublish;
     }
 }
