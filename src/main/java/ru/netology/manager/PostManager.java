4442b5ab1174ce7a3975d21e8a6f9aeed3421058
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-15 14:26:05 +0700

    Update PostManager.java

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -3,13 +3,12 @@ package ru.netology.manager;
 import ru.netology.domain.Post;
 
 public class PostManager {
-    private Post[] post;
 
-    public Post[] search() {
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
 
-    public void Delete() {
+    public void Delete(int ownerID, int postId) {
     }
 
 }
