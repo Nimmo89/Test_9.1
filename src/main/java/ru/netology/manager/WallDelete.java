0bca4664ee815a3cbc42144b5591c52e561267c7
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-15 13:13:06 +0700

    Create WallDelete.java

---
+++ src/main/java/ru/netology/manager/WallDelete.java
@@ -0,0 +1,6 @@
+package ru.netology.manager;
+
+public class WallDelete {
+    int ownerID = 254354;  //идентификатор пользователя или сообщества
+    int postId = 4225324; //идентификатор записи на стене
+}
